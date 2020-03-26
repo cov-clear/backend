c4fe01d73a14bde5b4828c968cb46be94343dd3a
Kostas Stamatoukos <kostas.stamatoukos@transferwise.com>
2020-03-26 10:55:09 +0000

    Throw error instead of invoking next

--- src/api/routes/user.ts
+++ src/api/routes/user.ts
@@ -6,27 +6,23 @@ import { ApiError } from '../ApiError';
 export default () => {
   const route = new AsyncRouter();
 
-  route.get(
-    '/users/:id',
-    async (req: Request, res: Response, next: (...things: any[]) => any) => {
-      const { id } = req.params;
+  route.get('/users/:id', async (req: Request, res: Response) => {
+    const { id } = req.params;
 
-      const user = await getUser.byId(id);
+    const user = await getUser.byId(id);
 
-      if (!user) {
-        next(new ApiError(404, 'user.not-found'));
-        return;
-      }
-
-      res
-        .json({
-          id: user.id().value(),
-          email: user.email().value(),
-          creationTime: user.creationTime(),
-        })
-        .status(200);
+    if (!user) {
+      throw new ApiError(404, 'user.not-found');
     }
-  );
+
+    res
+      .json({
+        id: user.id().value(),
+        email: user.email().value(),
+        creationTime: user.creationTime(),
+      })
+      .status(200);
+  });
 
   return route.middleware();
 };
