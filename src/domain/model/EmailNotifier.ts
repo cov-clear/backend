2b1f68efc94963547aca790fdcf9f68da994cbb8
Jose Galarza <igalarzab@gmail.com>
2020-03-27 12:50:26 +0900

    Send emails

---
+++ src/domain/model/EmailNotifier.ts
@@ -0,0 +1,5 @@
+import { Email } from './user/Email';
+
+export interface EmailNotifier {
+  send(from: Email, to: Email, subject: string, body: string): Promise<boolean>;
+}
---
+++ src/infrastructure/emails/LoggingEmailNotifier.ts
@@ -0,0 +1,15 @@
+import { Email } from '../../domain/model/user/Email';
+import { EmailNotifier } from '../../domain/model/EmailNotifier';
+import logger from '../../logger';
+
+export class LoggingEmailNotifier implements EmailNotifier {
+  async send(
+    from: Email,
+    to: Email,
+    subject: string,
+    body: string
+  ): Promise<boolean> {
+    logger.info(`Email to "${to.value}" with the body: ${body}`);
+    return true;
+  }
+}
