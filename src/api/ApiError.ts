7d079386a64b78afd76405b1c2740e550d26802e
Kostas Stamatoukos <kostas.stamatoukos@transferwise.com>
2020-03-27 10:21:43 +0000

    Improve ApiError

--- src/api/ApiError.ts
+++ src/api/ApiError.ts
@@ -1,12 +1,8 @@
-export const defaultStatusMessages: { [key: string]: string } = {
-  404: 'Not found',
-  500: 'Internal server error',
-};
-
 export class ApiError extends Error {
   constructor(
     public status: number = 500,
-    public message: string = defaultStatusMessages[500]
+    public code: string,
+    public message: string = code
   ) {
     super(message);
   }
