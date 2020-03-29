4d9621c763d6cea1c2c9edd2b194c2d9ec0dc3b6
Kostas Stamatoukos <kostas.stamatoukos@transferwise.com>
2020-03-29 13:26:37 +0100

    Remove unneeded authentication repository

--- src/domain/model/authentication/AuthenticationRepository.ts
+++
@@ -1,6 +0,0 @@
-import { Authentication } from './Authentication';
-import { User } from '../user/User';
-
-export interface AuthenticationRepository {
-  getAuthentication(user: User): Promise<Authentication>;
-}
