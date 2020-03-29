916cf17e72b2b1f1312f1152a6b32a75d9d44d19
Kostas Stamatoukos <kostas.stamatoukos@transferwise.com>
2020-03-29 00:15:17 +0000

    Create seed data to ease testing before prod launch

--- src/loaders/database.ts
+++ src/loaders/database.ts
@@ -1,10 +1,16 @@
-import { rollbackDatabase, migrateLatest } from '../database';
+import { migrateLatest, rollbackDatabase } from '../database';
 import logger from '../logger';
+import { createSeedDataForTestingPeriod } from '../database/seed';
 
 export async function migrateDatabase() {
   try {
+    //TODO: Remove before rolling out to production
     await rollbackDatabase();
+
     await migrateLatest();
+
+    //TODO: Remove before rolling out to production
+    await createSeedDataForTestingPeriod();
   } catch (error) {
     logger.error('Failed to apply migrations', error);
     process.exit(1);
