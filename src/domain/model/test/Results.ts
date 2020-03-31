9c29c1745b3a5adfcfc2b9154e8e6d5dfb6e014c
Harsh Sinha <harsh@transferwise.com>
2020-03-31 11:02:02 +0100

    Added results columns to test table and added testType seed data

--- src/domain/model/test/Results.ts
+++ src/domain/model/test/Results.ts
@@ -3,7 +3,7 @@ import { UserId } from '../user/UserId';
 export class Results {
   constructor(
     public createdBy: UserId,
-    readonly creationTime: Date = new Date(),
-    public details: object
+    public details: object,
+    readonly creationTime: Date = new Date()
   ) {}
 }
