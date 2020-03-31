7a52b7c2d752df67eb81daa66fb7ce34c2059ea1
Steve Pole <steve.pole@transferwise.com>
2020-03-31 14:35:36 +0100

    Implement test results persistence

--- src/infrastructure/persistence/PsqlTestRepository.test.ts
+++ src/infrastructure/persistence/PsqlTestRepository.test.ts
@@ -3,6 +3,7 @@ import { PsqlTestRepository } from './PsqlTestRepository';
 import { TestTypeId } from '../../domain/model/testType/TestTypeId';
 import { Test } from '../../domain/model/test/Test';
 import { TestId } from '../../domain/model/test/TestId';
+import { Results } from '../../domain/model/test/Results';
 import { User } from '../../domain/model/user/User';
 import { UserId } from '../../domain/model/user/UserId';
 import { Email } from '../../domain/model/user/Email';
@@ -17,7 +18,7 @@ describe('PsqlTestRepository', () => {
     await cleanupDatabase();
   });
 
-  it('inserts new and retrieves a test by id', async () => {
+  it('inserts a test without results and retrieves it by id', async () => {
     const test = await psqlTestRepository.save(
       new Test(new TestId(), new UserId(), new TestTypeId())
     );
@@ -25,6 +26,28 @@ describe('PsqlTestRepository', () => {
     const persistedTest = await psqlTestRepository.findById(test.id);
 
     expect(persistedTest).toEqual(test);
+  });
+
+  it('inserts a test with results and retrieves it by id', async () => {
+    const userId = new UserId();
+    const details = { a: 1 };
+    const results = new Results(userId, details);
+
+    const test = await psqlTestRepository.save(
+      new Test(new TestId(), userId, new TestTypeId(), results)
+    );
+
+    const persistedTest = await psqlTestRepository.findById(test.id)!;
+
+    if (persistedTest && persistedTest.results) {
+      expect(persistedTest).toBeDefined();
+      expect(persistedTest).toEqual(test);
+      expect(persistedTest.results).not.toBeNull();
+      expect(persistedTest.results).toBeDefined();
+      expect(persistedTest.results.details).toEqual(details);
+    } else {
+      expect(true).toBe(false);
+    }
   });
 
   it('inserts two new tests and retrieves them by User Id', async () => {
