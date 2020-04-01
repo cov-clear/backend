fa38f59c6790f8eeaf8b3d78f06727732303a044
Kostas Stamatoukos <kostas.stamatoukos@transferwise.com>
2020-04-01 11:14:08 +0100

    Split testResults from test table (#52)
    * Split testResults from test table
    
    * Extract common query part for tests

--- src/database/migrations/V1__addTest.ts
+++ src/database/migrations/V1__addTest.ts
@@ -1,21 +1,28 @@
 import knex from 'knex';
 
 const TEST_TABLE = 'test';
+const TEST_RESULTS_TABLE = 'test_results';
 
 export async function up(db: knex) {
+  await db.schema.createTable(TEST_TABLE, (table) => {
+    table.uuid('id').primary();
+    table.uuid('user_id').notNullable();
+    table.uuid('test_type_id').notNullable();
+    table.timestamp('creation_time').notNullable().index();
+  });
+
   await db.schema
-    .createTable(TEST_TABLE, (table) => {
+    .createTable(TEST_RESULTS_TABLE, (table) => {
       table.uuid('id').primary();
-      table.uuid('user_id').notNullable();
-      table.uuid('test_type_id').notNullable();
-      table.timestamp('creation_time').notNullable().index();
-      table.jsonb('results').nullable();
-      table.uuid('results_creator_id').nullable();
-      table.timestamp('results_creation_time').nullable();
+      table.uuid('test_id').unique();
+      table.uuid('creator_id').notNullable();
+      table.jsonb('details').nullable();
+      table.timestamp('creation_time').notNullable();
     })
-    .raw('CREATE INDEX IDX_RESULTS on "test" USING GIN (results)');
+    .raw('CREATE INDEX IDX_RESULTS on "test_results" USING GIN (details)');
 }
 
 export async function down(db: knex) {
   await db.schema.dropTable(TEST_TABLE);
+  await db.schema.dropTable(TEST_RESULTS_TABLE);
 }
--- src/infrastructure/persistence/PsqlTestRepository.test.ts
+++ src/infrastructure/persistence/PsqlTestRepository.test.ts
@@ -4,13 +4,10 @@ import { TestTypeId } from '../../domain/model/testType/TestTypeId';
 import { Test } from '../../domain/model/test/Test';
 import { TestId } from '../../domain/model/test/TestId';
 import { Results } from '../../domain/model/test/Results';
-import { User } from '../../domain/model/user/User';
 import { UserId } from '../../domain/model/user/UserId';
-import { Email } from '../../domain/model/user/Email';
 
 import { cleanupDatabase } from '../../test/cleanupDatabase';
 
-// TODO: Add results
 describe('PsqlTestRepository', () => {
   const psqlTestRepository = new PsqlTestRepository(database);
 
@@ -39,23 +36,30 @@ describe('PsqlTestRepository', () => {
 
     const persistedTest = await psqlTestRepository.findById(test.id)!;
 
-    if (persistedTest && persistedTest.results) {
-      expect(persistedTest).toBeDefined();
-      expect(persistedTest).toEqual(test);
-      expect(persistedTest.results).not.toBeNull();
-      expect(persistedTest.results).toBeDefined();
-      expect(persistedTest.results.details).toEqual(details);
-    } else {
-      expect(true).toBe(false);
-    }
+    expect(persistedTest).toEqual(test);
+    expect(persistedTest?.results).toBeDefined();
+    expect(persistedTest?.results).not.toBeNull();
+    expect(persistedTest?.results?.details).toEqual(details);
   });
 
   it('inserts two new tests and retrieves them by User Id', async () => {
     const userId = new UserId();
     const testTypeId = new TestTypeId();
 
-    const test1 = new Test(new TestId(), userId, testTypeId);
-    const test2 = new Test(new TestId(), userId, testTypeId);
+    const test1 = new Test(
+      new TestId(),
+      userId,
+      testTypeId,
+      undefined,
+      new Date(Date.now() - 10000)
+    );
+    const test2 = new Test(
+      new TestId(),
+      userId,
+      testTypeId,
+      undefined,
+      new Date(Date.now())
+    );
 
     await psqlTestRepository.save(test1);
     await psqlTestRepository.save(test2);
@@ -64,6 +68,21 @@ describe('PsqlTestRepository', () => {
 
     expect(persistedTests[0]).toEqual(test1);
     expect(persistedTests[1]).toEqual(test2);
+  });
+
+  it('updates a test with results', async () => {
+    const userId = new UserId();
+    const details = { a: 1 };
+    const test = await psqlTestRepository.save(
+      new Test(new TestId(), userId, new TestTypeId())
+    );
+
+    test.results = new Results(userId, details);
+    await psqlTestRepository.save(test);
+
+    const persistedTest = await psqlTestRepository.findById(test.id);
+
+    expect(persistedTest?.results).toEqual(test.results);
   });
 });
 
