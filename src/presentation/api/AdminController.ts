1588440d8a978f97a94d60c301675a0865ca03fa
Jose Galarza <igalarzab@gmail.com>
2020-04-06 02:47:45 +0900

    Adding permissions to the bulk user creation endpoint

--- src/presentation/api/AdminController.ts
+++ src/presentation/api/AdminController.ts
@@ -8,12 +8,14 @@ import { AuthenticatedRequest } from '../../api/AuthenticatedRequest';
 import { bulkCreateUsers } from '../../application/service/index';
 import { CreateUserCommand } from '../commands/users';
 import { UserTransformer } from '../transformers/users';
+import { hasPermission } from '../../api/middleware/hasPermission';
+import { BULK_CREATE_USERS } from '../../domain/model/authentication/Permissions';
 
 export class AdminController implements ApiController {
   public routes(): Router {
     const route = new AsyncRouter();
 
-    route.post('/users', isAuthenticated, async (req: AuthenticatedRequest, res: Response) => {
+    route.post('/users', hasPermission(BULK_CREATE_USERS), async (req: AuthenticatedRequest, res: Response) => {
       const command = req.body as CreateUserCommand[];
       const users = await bulkCreateUsers.execute(command);
 
