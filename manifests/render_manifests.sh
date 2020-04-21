2f73880acca90dd88d5eae31fc8bf940a31f75e8
Jose Galarza <igalarzab@gmail.com>
2020-04-21 10:30:53 +0100

    Add CD to EKS

--- manifests/render_manifests.sh
+++ manifests/render_manifests.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-if [ "$#" -ne 2 ]; then
-  echo "Usage: $0 environment[prod|staging] version" >&2
-  echo "Ex: $0 prod latest" >&2
+if [ "$#" -ne 3 ]; then
+  echo "Usage: $0 environment[prod|staging] version domain" >&2
+  echo "Ex: $0 prod latest app.cov-clear.com" >&2
   exit 1
 fi
 
-for file in namespace pdb service deployment
+for file in namespace pdb service ingress deployment
 do
-  manifest=`sed "s/\[\[env\]\]/$1/g;s/\[\[docker_tag\]\]/$2/g" $file.yaml`
+  manifest=`sed "s/\[\[env\]\]/$1/g;s/\[\[docker_tag\]\]/$2/g;s/\[\[domain\]\]/$3/g" $file.yaml`
   echo  "$manifest"
 done
