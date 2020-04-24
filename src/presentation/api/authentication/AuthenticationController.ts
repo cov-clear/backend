43d5f13f9d6ff0f4c29643539c704f9ecf027602
Uku Tammet <ukutammet@gmail.com>
2020-04-24 16:42:23 +0300

    Start adding dokobit auth controller

--- src/presentation/api/authentication/AuthenticationController.ts
+++ src/presentation/api/authentication/AuthenticationController.ts
@@ -3,11 +3,21 @@ import { BodyParam, JsonController, Post, UseAfter } from 'routing-controllers';
 import { AuthenticationErrorHandler } from './AuthenticationErrorHandler';
 import * as config from '../../../config';
 
+interface AuthenticationSession {
+  token: string;
+  url: string;
+}
+
+interface CreateAuthenticationSession {
+  execute(): Promise<AuthenticationSession>;
+}
+
 @JsonController('/v1/auth')
 @UseAfter(AuthenticationErrorHandler)
 export class AuthenticationController {
   private createMagicLink = createMagicLink;
   private exchangeAuthCode = exchangeAuthCode;
+  private createAuthenticationSession: CreateAuthenticationSession;
 
   @Post('/magic-links')
   async createNewMagicLink(@BodyParam('email') emailValue: string) {
@@ -23,6 +33,15 @@ export class AuthenticationController {
       response.code = magicLink.code.value;
     }
     return response;
+  }
+
+  @Post('/sessions')
+  async createNewAuthenticationSession() {
+    const authSession = await this.createAuthenticationSession.execute();
+    return {
+      token: authSession.token,
+      url: authSession.url,
+    };
   }
 
   @Post('/login')
