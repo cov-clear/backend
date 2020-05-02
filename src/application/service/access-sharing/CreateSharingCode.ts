282267a4e4d499dd1fa02cee09b9e592aeaee1c6
Steve Pole <steve.pole@transferwise.com>
2020-05-02 20:36:42 +0100

    Missed change from last commit

--- src/application/service/access-sharing/CreateSharingCode.ts
+++ src/application/service/access-sharing/CreateSharingCode.ts
@@ -5,8 +5,8 @@ import { UserId } from '../../../domain/model/user/UserId';
 export class CreateSharingCode {
   constructor(private sharingCodeRepository: SharingCodeRepository) {}
 
-  public async withUserId(userId: string, duration: number = 15): Promise<SharingCode> {
-    const sharingCode = new SharingCode(new UserId(userId));
+  public async withUserId(userId: string, accessDuration: number = 15): Promise<SharingCode> {
+    const sharingCode = new SharingCode(new UserId(userId), accessDuration);
     return this.sharingCodeRepository.save(sharingCode);
   }
 }
